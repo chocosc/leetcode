e25a86ff1c4295ff52c0c5512faa57fe721a7abd
김지수 <131653523+chocosc@users.noreply.github.com>
2023-06-29 16:21:27 +0900

    Time: 97 ms (42.23%), Space: 16.6 MB (28.62%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.py
@@ -1,5 +1,12 @@
 class Solution:
     def pickGifts(self, gifts: List[int], k: int) -> int:
+        # 1안
+        # for _ in range(k):
+        #     gifts[gifts.index(max(gifts))] = int(gifts[gifts.index(max(gifts))] ** 0.5)
+        # return sum(gifts)
+
         for _ in range(k):
-            gifts[gifts.index(max(gifts))] = int(gifts[gifts.index(max(gifts))] ** 0.5)
+            gifts = sorted(gifts)
+            gifts[-1] = int(gifts[-1] ** 0.5)
+        
         return sum(gifts)
