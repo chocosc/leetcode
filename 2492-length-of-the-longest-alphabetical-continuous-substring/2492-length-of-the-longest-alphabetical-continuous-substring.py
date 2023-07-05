67193db4ce80eef519549911ae66fefe371c31a7
김지수 <131653523+chocosc@users.noreply.github.com>
2023-07-05 09:15:07 +0900

    Time: 350 ms (73.75%), Space: 18.7 MB (5.42%) - LeetHub

--- 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.py
+++ 2492-length-of-the-longest-alphabetical-continuous-substring/2492-length-of-the-longest-alphabetical-continuous-substring.py
@@ -1,20 +1,21 @@
 class Solution:
     def longestContinuousSubstring(self, s: str) -> int:
-        ans = 1
+        cur_len = 1
         cur_max = 1
+        
         ord_s = []
         for word in s:
             ord_s.append(ord(word))
         
         if len(s) == 1:
-            return ans
+            return cur_max
         
         for i in range(1, len(ord_s)):
             if ord_s[i] - ord_s[i-1] == 1:
-                ans += 1
-                if ans > cur_max:
-                    cur_max = ans
+                cur_len += 1
+                if cur_len > cur_max:
+                    cur_max = cur_len
             else:
-                ans = 1
+                cur_len = 1
 
         return cur_max
