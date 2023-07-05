85a12f4362e327b0b3a5314f6f0950ea987382be
김지수 <131653523+chocosc@users.noreply.github.com>
2023-07-05 09:07:36 +0900

    Time: 56 ms (38.22%), Space: 16.3 MB (38.22%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxScore(self, s: str) -> int:
-        from collections import Counter
         ans = []
         for i in range(1,len(s)):
-            l = Counter(s[:i])
-            r = Counter(s[i:])
-            ans.append(Counter(l)['0'] + Counter(r)['1'])
+            l = s[:i]
+            r = s[i:]
+            ans.append(l.count('0') + r.count('1'))
             
         return max(ans)
